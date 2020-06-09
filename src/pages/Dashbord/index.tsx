cd9e36dd41206a69e882a7cfe3a98ba027b25c5e
Ruan Aragao <aragao.ruan@gmail.com>
2020-06-09 11:06:54 -0300

    feat: estilizando dashboard

--- src/pages/Dashbord/index.tsx
+++ src/pages/Dashbord/index.tsx
@@ -1,9 +1,62 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashbord: React.FC = () => {
-  return <Title>Explore reposotórios no GitHub</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explore reposotórios no GitHub</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositorio" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="*">
+          <img
+            src="https://avatars2.githubusercontent.com/u/16023843?s=460&u=9fda30f3a9d504cf35135b8c26f47f5bca750eee&v=4"
+            alt="Ruan"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="*">
+          <img
+            src="https://avatars2.githubusercontent.com/u/16023843?s=460&u=9fda30f3a9d504cf35135b8c26f47f5bca750eee&v=4"
+            alt="Ruan"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="*">
+          <img
+            src="https://avatars2.githubusercontent.com/u/16023843?s=460&u=9fda30f3a9d504cf35135b8c26f47f5bca750eee&v=4"
+            alt="Ruan"
+          />
+
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms! </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashbord;
